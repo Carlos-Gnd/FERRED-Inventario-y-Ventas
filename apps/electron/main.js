20cd3c3f2a397f565bf670c96dbbea606a3ee5d2
NelsonDev10 <nelsitorodriguez2006@gmail.com>
2026-04-16 18:52:59 -0600

    fix(sqlite): reescribir queries con prepared statements y hardening Electron

--- apps/electron/main.js
+++ apps/electron/main.js
@@ -58,7 +58,7 @@ function createWindow() {
       preload:          path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration:  false,
-      sandbox:          false,
+      sandbox:          true,
       webSecurity:      !IS_DEV,
     },
   });
